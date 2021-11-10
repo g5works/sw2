65bcd67820435fe94cee5894fb5dcde16939bd5e
Aryan Tadepalli <79216214+fireyinc@users.noreply.github.com>
2021-11-10 11:37:08 -0500

    message

--- Extra Files/JS/changeimage.js
+++ Extra Files/JS/changeimage.js
@@ -2,5 +2,5 @@ window.onload = changeimage();
 function changeimage(){
     var key1 = Math.floor(Math.random() * 10) + 1;
     var key1str = key1.toString();
-    document.getElementById("homeimage").src="Extra Files/Images/sw2img"+key1str+".webp";
+    document.getElementById("homeimage").src="Extra Files/Images/sw2img"+key1str+".gif";
 }
---
+++ Extra Files/JS/gallery.js
@@ -0,0 +1,25 @@
+var currentopenimage = 1;
+function changeimageforwardgallery(){
+    currentopenimage += 1;
+    if (currentopenimage > 10){
+        currentopenimage = 10;
+    } 
+    var key1str = currentopenimage.toString();
+    document.getElementById("imagern").innerHTML = key1str+"/10"
+    document.getElementById("downloadlink").href = "Extra Files/Images/sw2img"+key1str+".gif"
+    document.getElementById("downloadlink").download = "sw2img"+key1str+".gif"
+    document.getElementById("galleryprev").src="Extra Files/Images/sw2img"+key1str+".gif";
+}
+
+function changeimagebackwardgallery(){
+    currentopenimage -= 1;
+    if (currentopenimage <= 0){
+        currentopenimage = 1;
+    }
+    
+    var key1str = currentopenimage.toString();
+    document.getElementById("imagern").innerHTML = key1str+"/10"
+    document.getElementById("downloadlink").href = "Extra Files/Images/sw2img"+key1str+".gif"
+    document.getElementById("downloadlink").download = "sw2img"+key1str+".gif"
+    document.getElementById("galleryprev").src="Extra Files/Images/sw2img"+key1str+".gif";
+}
